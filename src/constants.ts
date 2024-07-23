a3023875bb5a2278436cf83f382c6af24e8ee34a
Austin Liu <austin.liu05@yahoo.com>
2024-07-22 21:57:20 -0500

    Refactored code base, removed symlink assets/

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,2 @@
-export const ASSETS_SKILLS_BASE_PATH = '../../assets/skills/';
-export const ASSETS_BASE_PATH = '../../assets/';
-
-
+export const ASSETS_SKILLS_BASE_PATH = `${process.env.PUBLIC_URL}/assets/skills/`;
+export const ASSETS_BASE_PATH = `${process.env.PUBLIC_URL}/assets/`;
