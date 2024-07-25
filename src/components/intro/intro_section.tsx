b478a9ae1ca47c6c7095a22d6f450bb9d169732c
Austin Liu <austin.liu05@yahoo.com>
2024-07-25 13:57:36 -0500

    Added headshot

--- src/components/intro/intro_section.tsx
+++ src/components/intro/intro_section.tsx
@@ -4,6 +4,9 @@ const IntroSection: React.FC = () => {
   return (
     <section className="section-intro">
         <div className="bio-container">
+          <div className="headshot-container">
+          <img className='headshot' src={`${process.env.PUBLIC_URL}/assets/headshot.jpg`} alt="Austin Liu"/>
+          </div>
           <h1>My Web Portfolio</h1>
           <p className="bio py-2">
             Hi there! I'm Austin Liu, a sophomore at Brown University pursuing a
