5151c9b06cb5eb3b3a0a9b36908bb55f7181a6a0
Austin Liu <austin.liu05@yahoo.com>
2024-09-05 16:38:21 -0400

    Edit the intro

--- src/components/intro/intro_section.tsx
+++ src/components/intro/intro_section.tsx
@@ -3,25 +3,23 @@ import "./intro_section.css";
 const IntroSection: React.FC = () => {
   return (
     <section className="section-intro">
-        <div className="bio-container">
-          <div className="headshot-container">
-          <img className='headshot' src={`${process.env.PUBLIC_URL}/assets/headshot.jpg`} alt="Austin Liu"/>
-          </div>
-          <h1>My Web Portfolio</h1>
-          <p className="bio py-2">
-            Hi there! I'm Austin Liu, a sophomore at Brown University pursuing a
-            dual degree in Computer Science and Economics, along with an
-            Entrepreneurship Certificate.
-          </p>
-          <div className="resume">
-            <a
-              href={`${process.env.PUBLIC_URL}/assets/AustinLiuResume.pdf`}
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <button>RESUME</button>
-            </a>
-          </div>
+      <div className="bio-container">
+        <div className="headshot-container">
+          <img className='headshot' src={`${process.env.PUBLIC_URL}/assets/headshot.jpg`} alt="Austin Liu" />
+        </div>
+        <h1>My Web Portfolio</h1>
+        <p className="bio py-2">
+          Hi! I'm Austin Liu, currently pursuing a dual degree in Computer Science and Economics at Brown University, with an additional focus on earning an Entrepreneurship Certificate.
+        </p>
+        <div className="resume">
+          <a
+            href={`${process.env.PUBLIC_URL}/assets/AustinLiuResume.pdf`}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <button>RESUME</button>
+          </a>
+        </div>
       </div>
     </section>
   );
