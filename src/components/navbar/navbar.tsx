4520d071f075b900c6cb84cfc9b62ec50080a839
Austin Liu <austin.liu05@yahoo.com>
2025-01-01 18:13:06 -0600

    Added text animation

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,7 +3,9 @@ import './navbar.css';
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <div className="nav-logo">Austin Liu</div>
+            <div className="nav-logo">
+                <span className="falling-text">Austin Liu</span>
+            </div>
             <ul className="nav-links">
                 <li><a href="#skills">Skills</a></li>
                 <li><a href="#projects">Projects</a></li>
