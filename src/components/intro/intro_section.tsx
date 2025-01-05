9705648a695476e894017c4a6edfb02e5ef58a49
Austin Liu <austin.liu05@yahoo.com>
2025-01-05 02:47:08 -0600

    Prevent word breaks in middle

--- src/components/intro/intro_section.tsx
+++ src/components/intro/intro_section.tsx
@@ -3,13 +3,15 @@ import "./intro_section.css";
 
 const IntroSection: React.FC = () => {
   const [titleText, setTitleText] = useState<JSX.Element[]>([]);
-  const [bioText, setBioText] = useState<JSX.Element[]>([]);
+  const [bioContent, setBioContent] = useState<JSX.Element[]>([]);
   const [isFlipped, setIsFlipped] = useState(false);
 
   useEffect(() => {
     const title = "austinliu.dev";
-    const bio = "Hi! I'm Austin Liu, currently pursuing a dual degree in Computer Science and Economics at Brown University, with an additional focus on earning an Entrepreneurship Certificate";
+    const bio =
+      "Hi! I'm Austin Liu, currently pursuing a dual degree in Computer Science and Economics at Brown University, with an additional focus on earning an Entrepreneurship Certificate";
 
+    // TITLE: Split into letters for continuous animation
     const titleLetters = title.split("").map((letter, index) => (
       <span
         key={`title-${index}`}
@@ -22,21 +24,20 @@ const IntroSection: React.FC = () => {
         {letter === " " ? "\u00A0" : letter}
       </span>
     ));
-
-    const bioLetters = bio.split("").map((letter, index) => (
-      <span
-        key={`bio-${index}`}
-        className="hover-text-bio"
-        style={{
-          transitionDelay: `0s`,  // Stagger the hover effect
-        }}
-      >
-        {letter === " " ? "\u00A0" : letter}
-      </span>
-    ));
-
     setTitleText(titleLetters);
-    setBioText(bioLetters);
+
+    // BIO: Split into words, then each word into letters, so line breaks only occur between words
+    const bioWords = bio.split(" ").map((word, i) => (
+      <span key={`word-${i}`} className="word">
+        {word.split("").map((letter, j) => (
+          <span key={`word-${i}-letter-${j}`} className="hover-text-bio">
+            {letter}
+          </span>
+        ))}
+        <span>&nbsp;</span> {/* Add space between words */}
+      </span>
+    )); 
+    setBioContent(bioWords);
   }, []);
 
   const handleCardClick = () => {
@@ -62,7 +63,9 @@ const IntroSection: React.FC = () => {
             <div className="bio-text">
               <h2>About Me</h2>
               <p>
-                When I'm not working, you'll probably find me playing volleyball on Brown’s men's varsity team, playing fingerstyle guitar for friends, or hunting down new food places to try.
+                When I'm not working, you'll probably find me playing volleyball on Brown’s men's
+                varsity team, playing fingerstyle guitar for friends, or hunting down new food places
+                to try.
               </p>
             </div>
           </div>
@@ -70,10 +73,14 @@ const IntroSection: React.FC = () => {
       </div>
 
       <div className="bio-container">
+        {/* Title text with continuous animation */}
         <h1 className="hover-text">{titleText}</h1>
-        <p className="bio py-2">
-          {bioText}
+
+        {/* Bio text, word-wrapped */}
+        <p className="bio">
+          {bioContent}
         </p>
+
         <div className="resume">
           <a
             href={`${process.env.PUBLIC_URL}/assets/AustinLiuResume.pdf`}
