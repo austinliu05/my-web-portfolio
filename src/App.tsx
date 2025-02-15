80f4b2e4acd4747d15c8e3170b3698056f2b99b4
Austin Liu <austin.liu05@yahoo.com>
2025-02-15 18:35:19 -0500

    Added custom cursor

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import './App.css';
 import IntroSection from './components/intro/intro_section';
 import ProjectsSections from './components/projects/projects_section';
 import ExperiencesSection from './components/experiences/experiences_section';
-import Skills from './components/skills/skills'
-import Footer from './components/footer/footer'
+import Skills from './components/skills/skills';
+import Footer from './components/footer/footer';
 import Navbar from './components/navbar/navbar';
 import SkillsCarousel from './components/skills/skills_carousel';
+import CustomCursor from './components/customCursor';
 
 function App() {
   return (
     <div className="App">
+      <CustomCursor />
       <Navbar />
       <div>
         <IntroSection />
         <hr className="separator" />
-        <SkillsCarousel/>
+        <SkillsCarousel />
         <hr className="separator" />
         <ProjectsSections />
         <hr className="separator" />
---
+++ src/components/customCursor.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import './customCursor.css';
+
+const CustomCursor = () => {
+    const [cursorX, setCursorX] = useState(0);
+    const [cursorY, setCursorY] = useState(0);
+    const [isClicking, setIsClicking] = useState(false);
+
+    // Update the cursor position on mouse move
+    const move = (e:any) => {
+        setCursorX(e.clientX);
+        setCursorY(e.clientY);
+    };
+
+    const handleMouseDown = () => {
+        setIsClicking(true);
+    };
+
+    const handleMouseUp = () => {
+        setIsClicking(false);
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousemove', move);
+        document.addEventListener('mousedown', handleMouseDown);
+        document.addEventListener('mouseup', handleMouseUp);
+
+        return () => {
+            document.removeEventListener('mousemove', move);
+            document.removeEventListener('mousedown', handleMouseDown);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, []);
+
+    return (
+        <>
+            <div
+                className="custom-cursor"
+                style={{
+                    left: `${cursorX}px`,
+                    top: `${cursorY}px`,
+                }}
+            />
+            {/* <div
+                className="custom-cursor-border"
+                style={{
+                    left: `${cursorX}px`,
+                    top: `${cursorY}px`,
+                }}
+            /> */}
+        </>
+    );
+};
+
+export default CustomCursor;
