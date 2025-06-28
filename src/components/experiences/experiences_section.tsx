a2dbf740c37776803c0690efd7b074c7f380d81d
Austin Liu <austin.liu05@yahoo.com>
2025-06-28 13:23:23 -0400

    Updated RESUME

--- src/components/experiences/experiences_section.tsx
+++ src/components/experiences/experiences_section.tsx
@@ -17,7 +17,7 @@ const experienceItems = [
     techStack: ["languages/c-sharp.png", "languages/sql.png"]
   },
   {
-    title: "Stealth Startup",
+    title: "Helios Technologies",
     role: "Software Engineer Intern",
     date: "Feb.  2025 - May 2025",
     description: "AI chatbots. Creating user friendly UI, optimizing backend RAG workflows and performing QA testing on production deployments.",
